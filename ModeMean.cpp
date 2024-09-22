4337330d955fa3b015ba6f814b0a767e45603aa4
Sekacorn <43796712+sekacorn@users.noreply.github.com>
2024-09-22 06:37:00 -0400

    Update ModeMean.cpp
    added a  few visuals a basic user interfaced to interact with

--- ModeMean.cpp
+++ ModeMean.cpp
@@ -1,124 +1,97 @@
 /*Author: corn
-finds mode(if even number of element finds the average of the two) mean and median given that the user inputs a series of numbers*/
+Finds mode (if even number of elements finds the average of the two), mean, and median given that the user inputs a series of numbers*/
 
 #include <iostream>
-#include<stdio.h>
-#include<time.h>
+#include <vector>
+#include <algorithm>
+#include <map>
 
 using namespace std;
 
-void arraySort(double array[], int numOfInts){
-// Sorts the array in numerical ooder
-for(int i=0; i<numOfInts; i++){
-         for(int a=0; a<numOfInts-1; a++){
-             if(array[a]>array[a+1]){//if an elemetn is smaller that previous put it in temp
-                 double temp = array[a+1];
-                 array[a+1] = array[a];// place the previous element into the next
-                 array[a] = temp;//get the smaller etlement and place in the previous spot that the bigger elemen
-             }
-         }
-     }
-    cout<<"Sort 1"<<endl;
-for(int i=0;i<numOfInts; i++){
-cout<<array[i]<<", ";
-}
-cout<<endl;
-
-}
-void arraySort2(double array[], int numOfInts){
-//Sort Array without using []
-for(int i= 0; i<numOfInts;i++){
- for(int a =0; a<numOfInts-1; a++){
-	if(*(array+a)>*(array+a+1)){//if an elemetn is smaller that previous put it in temp
-	double *temp= (array+a+1);// 
-	*(array+1)=*(array+a);// place the previous element into the next
-	*(array+a)=*temp;//get the smaller etlement and place in the previous spot that the bigger elemen
-		}
-	}
-	
-	}
-
-	cout<<"Sort 2"<<endl;
-	for(int i=0;i<numOfInts; i++){
-	cout<<*(array+i)<<", ";}
-	cout<<endl;
+// Function to sort an array
+void arraySort(double array[], int numOfInts) {
+    // Sort the array in numerical order using std::sort
+    sort(array, array + numOfInts);
+    cout << "Sorted Array: ";
+    for (int i = 0; i < numOfInts; i++) {
+        cout << array[i] << ", ";
+    }
+    cout << endl;
 }
 
-
-//////////////////////////////////////
-
+// Function to calculate mode
 void mode(double array[], int num) {
-  double* Ptr = new double[num];
-    // alocate a new array in memory of the same size (round about way of defining number of elements by a variable)
+    map<double, int> freqMap;
     for (int i = 0; i < num; i++) {
-        Ptr[i] = 0;//initialize each element to 0
-        int j = 0;//
-        while (j < i && array[i] !=array[j]) {
-            if (array[i] != array[j]) {//Compares element if they don't   repeat Carbon print array Ptr is increased
-                j++;
-            }
-        }
-        (Ptr[j])++;
-  }
- int repeat2 = 0;
-    for (int i = 1; i < num; i++) {// Checks for the number of elements that lowest in the carbon print array
-        if (Ptr[i] > Ptr[repeat2]) {
-            repeat2 = i;// sets that index to repeat
+        freqMap[array[i]]++; // Count occurrences of each element
+    }
+
+    int maxCount = 0;
+    double modeValue = -1;
+    bool hasMode = false;
+
+    for (auto it : freqMap) {
+        if (it.second > maxCount) {
+            maxCount = it.second;
+            modeValue = it.first;
+            hasMode = true;
         }
     }
-    if(Ptr[repeat2]>1){
-    cout<< "Mode is " << array[repeat2] << endl; //set that repeat index number  into the actual array 
-    }
-    else{
-    cout<<"There is no Mode"<<endl;
-        
+
+    if (hasMode && maxCount > 1) {
+        cout << "Mode is " << modeValue << " (appeared " << maxCount << " times)" << endl;
+    } else {
+        cout << "There is no mode." << endl;
     }
 }
-////////////////////////////////////
 
-void median(double array[], int num){
-    double average=0;
-    if(num % 2 != 0){// is the # of elements odd?
-        int temp = ((num+1)/2)-1; //finds the middle index and places it in the actual array
-        cout << "The median is " << array[temp] << endl;
+// Function to calculate median
+void median(double array[], int num) {
+    if (num % 2 != 0) {
+        cout << "The median is " << array[num / 2] << endl;
+    } else {
+        double average = (array[num / 2] + array[(num / 2) - 1]) / 2.0;
+        cout << "The median is " << average << endl;
     }
-    else{// then it's even! :)
- 	average=(array[num/2]+array[(num/2)-1])/2;//finds the middle index  and places it in the actual array
-        cout << "Median is "<<average << endl;
-    }
-   
 }
 
-int  main()
-{   int numOfInts=40;
-	double average;
-    double sum;
+// Function to calculate mean
+double mean(double array[], int num) {
+    double sum = 0;
+    for (int i = 0; i < num; i++) {
+        sum += array[i];
+    }
+    return sum / num;
+}
 
-    
-    // initialise
-    sum = 0.0;
-    srand (time(NULL));
-   
-    numOfInts = (rand()%12+5); //size array with numbers between 5 and 12'
+int main() {
+    int numOfInts;
+
+    // Ask user for number of integers
+    cout << "Enter the number of integers: ";
+    cin >> numOfInts;
+
+    if (numOfInts <= 0) {
+        cout << "Number of integers must be positive." << endl;
+        return 1;
+    }
+
     double array[numOfInts];
-    cout<<numOfInts<<endl;
-	
-    for(int a=0; a<numOfInts; a++){
-        array[a] = (rand()%30);//Randomly initialize the array with numbers in and between 0 and 30
- 
-	sum=array[a]+sum;
 
-   }
-   
-        average = sum / numOfInts;
-        cout << "Mean is  " << average << endl;
-       
-	mode(array, numOfInts);
-	arraySort(array,numOfInts);
-	 median(array, numOfInts);
-	arraySort2(array, numOfInts);
+    // Ask user to input the numbers
+    cout << "Enter " << numOfInts << " numbers:" << endl;
+    for (int i = 0; i < numOfInts; i++) {
+        cin >> array[i];
+    }
 
-    
-return 0;
+    // Calculate and display mean
+    double average = mean(array, numOfInts);
+    cout << "Mean is " << average << endl;
 
+    // Sort the array and display median and mode
+    arraySort(array, numOfInts);
+    median(array, numOfInts);
+    mode(array, numOfInts);
+
+    return 0;
 }
